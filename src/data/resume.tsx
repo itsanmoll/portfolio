7936630c0cdb3ec60f9e940fc7a3c9a3e17a0bdd
Anmol Bhardwaj <anmolbhardwajinv@gmail.com>
2025-08-12 17:23:03 +0530

    little changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -193,21 +193,23 @@ export const DATA = {
       dates: "Jan 2024 - Feb 2024",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "Developed an interactive satellite tracking application leveraging CesiumJS for immersive 3D visualization of real-time satellite positions. Implemented precise orbit and position estimation using Two-Line Element (TLE) data, ensuring high accuracy in satellite path prediction. Enhanced user experience with dynamic globe rendering, real-time updates, and intuitive navigation controls for tracking multiple satellites simultaneously.",
       technologies: [
         "ReactJs",
+        "CesiumJs",
         "Typescript",
+        "Django",
+        "Docker",
         "MySQL",
         "TailwindCSS",
         "Daisy UI",
-        "Magic UI",
       ],
       links: [
-        {
-          type: "Website",
-          href: "",
-          icon: <Icons.globe className="size-3" />,
-        },
+        // {
+        //   type: "Website",
+        //   href: "",
+        //   icon: <Icons.globe className="size-3" />,
+        // },
       ],
       image: "",
       video:
